876ddf397e0c5697a8e754e2d23abcbdac8d753d
jiangxy9 <jiangxy9@lenovo.com>
2019-03-22 17:34:38 +0800

    init

---
+++ src/main/java/jiang/device_upgrade/DeviceUpgradeApplication.java
@@ -0,0 +1,13 @@
+package jiang.device_upgrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceUpgradeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeviceUpgradeApplication.class, args);
+    }
+
+}
